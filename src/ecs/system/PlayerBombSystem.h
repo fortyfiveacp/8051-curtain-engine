b9fa88c56504025326614ce827154481dea6f731
CyrusVillaruz <91701334+CyrusVillaruz@users.noreply.github.com>
2026-04-06 00:01:57 -0700

    Added fade-in/out for bombs

--- src/ecs/system/PlayerBombSystem.h
+++ src/ecs/system/PlayerBombSystem.h
@@ -10,8 +10,18 @@ public:
         for (auto& entity : entities) {
             if (entity->hasComponent<PlayerBomb>()) {
                 auto& bomb = entity->getComponent<PlayerBomb>();
+                auto& fadeOut = entity->getComponent<Fade>();
 
                 bomb.timer += dt;
+
+                if (bomb.timer >= 2.0f && fadeOut.endingAlpha != 0) {
+                    fadeOut.isFading = true;
+                    fadeOut.startingAlpha = 100;
+                    fadeOut.endingAlpha = 0;
+                    fadeOut.fadeDuration = 1.0f;
+                    fadeOut.durationTimer = 0;
+                }
+
                 if (bomb.timer >= bomb.duration) {
                     entity->destroy();
                 }
--- src/utils/PlayerBombFactory.h
+++ src/utils/PlayerBombFactory.h
@@ -13,7 +13,6 @@ public:
         bomb.addComponent<Velocity>(Vector2D(0, -1), 100.0f);
 
         SDL_Texture* tex = TextureManager::load("../asset/bomb.png");
-        SDL_SetTextureAlphaMod(tex, 100);
         SDL_FRect src = {0, 0, 64, 64};
         SDL_FRect dst = {0, 0, size, size};
         bomb.addComponent<Sprite>(tex, src, dst, RenderLayer::World);
@@ -22,5 +21,11 @@ public:
         float radius = size / 2.0f;
         col.radius = radius;
         col.offset = { radius, radius };
+
+        auto& fadeIn = bomb.addComponent<Fade>();
+        fadeIn.isFading = true;
+        fadeIn.startingAlpha = 0;
+        fadeIn.endingAlpha = 100;
+        fadeIn.fadeDuration = 0.5f;
     }
 };
