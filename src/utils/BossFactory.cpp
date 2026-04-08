c33f7a2e5ee9fefa6ebcbdf69e43e86656bd17f1
CyrusVillaruz <91701334+CyrusVillaruz@users.noreply.github.com>
2026-04-07 22:20:05 -0700

    Implemented basic emitter danmaku (currently, both emitters use the same pattern)

--- src/utils/BossFactory.cpp
+++ src/utils/BossFactory.cpp
@@ -1,4 +1,6 @@
 #include "BossFactory.h"
+
+#include "DanmakuFactory.h"
 #include "TransformUtils.h"
 #include "World.h"
 #include "manager/AssetManager.h"
@@ -25,26 +27,22 @@ void BossFactory::buildStageBoss(Entity &entity, World &world, const Boss &bossD
     entity.addComponent<Children>();
     entity.addComponent<Boss>(bossData);
 
-    for (const auto& offset : emitterOffsets) {
-        createChildEmitter(entity, world, offset);
+    if (!bossData.phaseList.empty()) {
+        for (const auto& offset : emitterOffsets) {
+            createChildEmitter(entity, world, offset, bossData.phaseList[0].pattern);
+        }
     }
 }
 
-void BossFactory::createChildEmitter(Entity &parent, World &world, Vector2D offset) {
+void BossFactory::createChildEmitter(Entity &parent, World &world, Vector2D offset, const DanmakuPattern& pattern) {
     auto& emitter = world.createDeferredEntity();
     auto& pTransform = parent.getComponent<Transform>();
     auto& childrenComp = parent.getComponent<Children>();
 
     emitter.addComponent<Transform>().position = pTransform.position + offset;
     emitter.addComponent<Parent>().parent = &parent;
-    // emitter.addComponent<RadialSpawner>(); // TODO: read from the XML for danmaku configurations
 
-    // TODO: remove once danmaku can be successfully spawned from emitters.
-    SDL_Texture* tex = TextureManager::load("../asset/animations/sanae_anim.png");
-    SDL_FRect src {0, 0, 63, 65};
-    SDL_FRect dst {0, 0, 63, 65};
-    Vector2D pivotOffset = Vector2D(dst.w / 2.0f, dst.h / 2.0f);
-    emitter.addComponent<Sprite>(tex, src, dst, RenderLayer::World, pivotOffset);
+    DanmakuFactory::buildDanmaku(emitter, world, pattern);
 
     childrenComp.children.push_back(&emitter);
 }
